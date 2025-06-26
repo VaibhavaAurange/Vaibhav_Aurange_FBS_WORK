0cb09230427cb6f53caf78335309a4e28f692f17
VaibhavaAurange <vaibhavaurange@gmail.com>
2025-06-26 12:41:22 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignment 1/A0-10.c
@@ -0,0 +1,12 @@
+void main()
+{
+	float  m1=69;
+	float  m2=78;
+	float  m3=88;
+	float  m4=85;
+	float  m5=77;
+	float  Total=m1+m2+m3+m4+m5;
+	float Percentage=(Total/500)*100;
+	printf("Total marks %f ,Percentage %f", Total,Percentage);
+	
+}
