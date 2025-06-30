6dd484d119c26557c7f34d85f16780474066ad08
VaibhavaAurange <vaibhavaurange@gmail.com>
2025-06-30 14:33:25 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignments  1/voting.c
@@ -0,0 +1,14 @@
+void main()
+{
+	int age;
+	printf("Enter the age ");
+	scanf("%d", &age);
+	if (age>=18)
+	{
+		printf("Eligible for the vote");
+	}
+		else
+		{
+			printf ("Not Eligible for the vote");
+		}
+}
