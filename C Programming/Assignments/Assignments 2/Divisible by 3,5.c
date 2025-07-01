250afc6e090d32bdac289ebe54a8a58f50e402ce
VaibhavaAurange <vaibhavaurange@gmail.com>
2025-07-01 17:48:37 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignments 2/Divisible by 3,5.c
@@ -0,0 +1,29 @@
+//Accept a number and check if it is divisible by 3, 5, or both.
+void main()
+{
+	int no;
+	printf("Enter the no ");
+	scanf("%d",&no);
+	if(no%3==0 && no%5==0)
+	{
+		printf("Divisible by both");
+	}
+	else
+	{
+		if(no%5==0)
+		{
+			printf("Divisible by 5 but not by 3");
+		}
+		else
+		{
+			if(no%3==0)
+			{
+				printf("Divisible by 3 but not 5");
+			}
+			else
+			{
+				printf("Divisible by none");
+			}
+		}
+	}
+}
