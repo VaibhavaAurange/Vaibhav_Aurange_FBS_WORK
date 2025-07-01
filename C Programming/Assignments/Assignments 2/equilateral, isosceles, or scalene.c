27ee760a459ac97d16c1ff92511ffb17dd1300ba
VaibhavaAurange <vaibhavaurange@gmail.com>
2025-07-01 17:46:38 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignments 2/equilateral, isosceles, or scalene.c
@@ -0,0 +1,22 @@
+//Accept three sides of a triangle from the user and determine whether the triangle isequilateral, isosceles, or scalene.
+void main()  
+{  
+    float a, b, c;  
+  
+    printf("Enter values for a, b ,c\n");  
+    scanf("%f%f%f", &a, &b, &c);  
+  
+        if(a == b && b == c)  
+        {  
+            printf("It's an Equilateral Triangle\n");  
+        }  
+        else if(a == b || a == c || b == c)  
+        {  
+            printf("It's an Isosceles Triangle\n");  
+        }  
+        else  
+        {  
+            printf("It's a Scalene Triangle\n");  
+        }  
+  
+}  
