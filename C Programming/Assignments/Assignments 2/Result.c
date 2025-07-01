4787dd164c6657e945e5738cf6fe6fdd1d350206
VaibhavaAurange <vaibhavaurange@gmail.com>
2025-07-01 17:47:57 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignments 2/Result.c
@@ -0,0 +1,39 @@
+//show the result based on marks
+void main()
+{
+	int marks;
+	printf("Enter the marks");
+	scanf("%d",&marks);
+	if(marks>75)
+	{
+		printf("Distinction");
+	}
+	else
+	{
+		if (marks>65)
+		{
+			printf("First class");
+		}
+		else
+		{
+			if(marks>55)
+			{
+				printf("Second class");
+			}
+			else
+			{
+				if(marks>=40)
+				{
+					printf("Pass class");
+				}
+				else
+				{
+					if(marks<40)
+					{
+						printf("Fail");
+					}
+				}
+			}
+		}
+	}
+}
