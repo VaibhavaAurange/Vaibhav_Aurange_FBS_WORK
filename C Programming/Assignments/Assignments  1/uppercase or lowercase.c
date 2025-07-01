b616cd80350d1588ddb50d001256672491939056
VaibhavaAurange <vaibhavaurange@gmail.com>
2025-07-01 13:04:24 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignments  1/uppercase or lowercase.c
@@ -0,0 +1,15 @@
+//character is uppercase or lowercase.
+void main()
+{
+	char ch;
+	printf("Enter the Character ");
+	scanf("%c",&ch);
+	if(ch>='A'&& ch<='Z')
+	{
+		printf("Character is Uppercase ");
+	}
+	else
+	{ 
+	    printf("Character is Lowercase ");
+	}
+}
