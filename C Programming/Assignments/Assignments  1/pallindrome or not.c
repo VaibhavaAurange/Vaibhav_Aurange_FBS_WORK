a6987fd0ce9974228572397415a98014d6d553f6
VaibhavaAurange <vaibhavaurange@gmail.com>
2025-07-01 13:03:30 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignments  1/pallindrome or not.c
@@ -0,0 +1,21 @@
+void main ()
+{
+	int no=121;
+	int r1,r2,r3,rev;
+	int q1;
+	r1=no%10;
+	q1=no/10;
+	r2=q1%10;
+	r3=q1/10;
+	
+	rev=r1*100+r2*10+r3;
+	printf("%d",rev);
+	if(rev==no)
+	{
+		printf("The no is Pallindrome ");
+	}
+	else
+	{
+		printf("The no is not Pallindrome");
+	}
+}
