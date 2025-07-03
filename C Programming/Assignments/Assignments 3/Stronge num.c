879a6da249d89f6ee2f0f7e1bb245082a3db01fc
VaibhavaAurange <vaibhavaurange@gmail.com>
2025-07-03 12:40:27 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignments 3/Stronge num.c
@@ -0,0 +1,27 @@
+// Stronge num or not
+void main()
+{
+	int n,sum=0;
+	int temp, num;
+	printf("Enter the no: ");
+	scanf("%d",&n);
+	temp=n;
+	
+	while (temp!=0)
+	{
+		num=temp%10;
+		int fact=1;
+		int i=num;
+		while(i>1)
+		{
+			fact=fact*i;
+			i--;
+		}
+		sum=sum+fact;
+		temp=temp/10;	
+	}
+	if (sum==n)
+	printf("%d The Number is a stronge ",n);
+	else
+	printf("%d The Number is not stronge ",n);
+}
