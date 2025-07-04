66c66db035ed7faba4fb32f99684613f209330d8
VaibhavaAurange <vaibhavaurange@gmail.com>
2025-07-04 12:34:20 +0530

    Add files via upload

---
+++ C Programming/Test/Test 2/Q2.c
@@ -0,0 +1,20 @@
+void main()
+{
+	int num;
+	printf("Enter the num: ");
+	scanf("%d",&num);
+	
+	if(num>0)
+	{
+		printf("num is positive\n");
+	}
+	else if(num<0)
+	{
+		printf("num is negative\n");
+	}
+	else 
+	{
+	  printf("num is neutral\n");	
+	}
+	
+}
