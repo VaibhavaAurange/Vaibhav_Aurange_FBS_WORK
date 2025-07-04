1d79f454fb3391d89a5f4d576730b3d57d8e5914
VaibhavaAurange <vaibhavaurange@gmail.com>
2025-07-04 16:31:29 +0530

    Add files via upload

---
+++ C Programming/Test/Test 2/Q1.c
@@ -0,0 +1,25 @@
+//Electricity bill
+
+void main()
+{
+    int units;
+    float bill = 0;
+
+    printf("Enter the units : ");
+    scanf("%d", &units);
+
+    if (units <= 50)
+    {
+        bill = units * 30;
+    }
+    else if (units <= 150)
+    {
+        bill = (50 * 30) + ((units - 50) * 40);
+    }
+    else
+    {
+        bill = (50 * 30) + (100 * 40) + ((units - 150) * 50);
+    }
+
+    printf("Total Electricity Bill: %f", bill);
+}
