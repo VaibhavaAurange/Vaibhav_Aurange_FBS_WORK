b9f7ef0147566252e2d55b0f2adbd37f8ab73046
VaibhavaAurange <vaibhavaurange@gmail.com>
2025-07-05 19:45:37 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignments 5/Q9.c
@@ -0,0 +1,20 @@
+// print hollow square pattern
+void main()
+{
+	int n;
+	printf("Enter the num n: ");
+	scanf("%d",&n);
+	
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n;j++)
+		 {
+		   if(i==1||i==n||j==1||j==n)
+		   printf("* ");
+		   else
+		   printf("  ");
+       	} 
+		   printf("\n");
+
+	}
+}
