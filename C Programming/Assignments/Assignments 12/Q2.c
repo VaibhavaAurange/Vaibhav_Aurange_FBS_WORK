06be32a7febc0c70b765a3daa55ebc716535d554
VaibhavaAurange <vaibhavaurange@gmail.com>
2025-07-23 22:08:15 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignments 12/Q2.c
@@ -0,0 +1,17 @@
+//WAP Replace all Occurrences of ‘a’ with $ in a String
+#include <stdio.h>
+void main() 
+{
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s",str); 
+    
+    for (int i=0; str[i]!='\0';i++)
+	 {
+        if (str[i]=='a') 
+		{
+            str[i]='$';  
+        }
+    }
+    printf("New string: %s\n",str);
+}
