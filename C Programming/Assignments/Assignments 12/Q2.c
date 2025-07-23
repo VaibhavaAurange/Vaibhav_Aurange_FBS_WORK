20f879292b83a953ef67a496685e03a213e736c5
VaibhavaAurange <vaibhavaurange@gmail.com>
2025-07-23 22:11:53 +0530

    Update Q2.c

--- C Programming/Assignments/Assignments 12/Q2.c
+++ C Programming/Assignments/Assignments 12/Q2.c
@@ -7,11 +7,11 @@ void main()
     scanf("%s",str); 
     
     for (int i=0; str[i]!='\0';i++)
-	 {
+ {
         if (str[i]=='a') 
-		{
+	{
             str[i]='$';  
         }
-    }
+ }
     printf("New string: %s\n",str);
 }
